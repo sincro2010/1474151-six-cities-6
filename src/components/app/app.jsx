45f3595c9d521acca9dd8d7cc67e71a5665ad289
sincro2010 <svobodagrani@gmail.com>
2021-02-15 19:38:37 +0500

    Исправления разметки

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -4,29 +4,29 @@ import Header from '../header/header';
 import MainPage from '../main-page/main-page';
 
 const App = (props) => {
-    const {propertiesNumber} = props;
-    const {placeData} = props;
+  const {propertiesNumber} = props;
+  const {placeData} = props;
 
-    return  (
-        <React.Fragment>
-            <Header/>
-            <MainPage propertiesNumber={propertiesNumber} places={placeData}/>
-        </React.Fragment>
-    );
+  return (
+    <React.Fragment>
+      <Header/>
+      <MainPage propertiesNumber={propertiesNumber} places={placeData}/>
+    </React.Fragment>
+  );
 };
 
-MainPage.propTypes = {
-    propertiesNumber: PropTypes.number.isRequired,
-    places: PropTypes.arrayOf(
-        PropTypes.shape({
-            title: PropTypes.string.isRequired,
-            image: PropTypes.string.isRequired,
-            type: PropTypes.string.isRequired,
-            price: PropTypes.number.isRequired,
-            rating: PropTypes.number,
-            isPremium: PropTypes.bool
-        })
-    ).isRequired
-  };
+App.propTypes = {
+  propertiesNumber: PropTypes.number.isRequired,
+  placeData: PropTypes.arrayOf(
+      PropTypes.shape({
+        title: PropTypes.string.isRequired,
+        image: PropTypes.string.isRequired,
+        type: PropTypes.string.isRequired,
+        price: PropTypes.number.isRequired,
+        rating: PropTypes.number,
+        isPremium: PropTypes.bool
+      })
+  ).isRequired
+};
 
 export default App;
--- src/components/place/place.jsx
+++ src/components/place/place.jsx
@@ -1,47 +1,55 @@
 import React from 'react';
+import PropTypes from "prop-types";
 
 const Place = (props) => {
-    return (
-
-        <article className="cities__place-card place-card">
-              {props.isPremium && (
-                  <div className="place-card__mark">
-                  <span>Premium</span>
-                </div>
-              )}
-        <div className="cities__image-wrapper place-card__image-wrapper">
-            <a href="#">
-            <img className="place-card__image" src={`img/${props.image}`} width="260" height="200" alt="Place image"/>
-            </a>
+  return (
+    <article className="cities__place-card place-card">
+      {props.isPremium && (
+        <div className="place-card__mark">
+          <span>Premium</span>
         </div>
-        <div className="place-card__info">
-            <div className="place-card__price-wrapper">
-            <div className="place-card__price">
-                <b className="place-card__price-value">&euro;{props.price}</b>
-                <span className="place-card__price-text">&#47;&nbsp;night</span>
-            </div>
-            <button className="place-card__bookmark-button place-card__bookmark-button--active button" type="button">
-                <svg className="place-card__bookmark-icon" width="18" height="19">
-                <use xlinkHref="#icon-bookmark"></use>
-                </svg>
-                <span className="visually-hidden">In bookmarks</span>
-            </button>
-            </div>
-            <div className="place-card__rating rating">
-            <div className="place-card__stars rating__stars">
-                <span style={{width: `${props.rating}%`}}></span>
-                <span className="visually-hidden">{props.rating}</span>
-            </div>
-            </div>
-            <h2 className="place-card__name">
-            <a href="#">{props.title}</a>
-            </h2>
-            <p className="place-card__type">{props.type}</p>
+      )}
+      <div className="cities__image-wrapper place-card__image-wrapper">
+        <a href="#">
+          <img className="place-card__image" src={`img/${props.image}`} width="260" height="200" alt="Place image"/>
+        </a>
+      </div>
+      <div className="place-card__info">
+        <div className="place-card__price-wrapper">
+          <div className="place-card__price">
+            <b className="place-card__price-value">&euro;{props.price}</b>
+            <span className="place-card__price-text">&#47;&nbsp;night</span>
+          </div>
+          <button className="place-card__bookmark-button place-card__bookmark-button--active button" type="button">
+            <svg className="place-card__bookmark-icon" width="18" height="19">
+              <use xlinkHref="#icon-bookmark"></use>
+            </svg>
+            <span className="visually-hidden">In bookmarks</span>
+          </button>
         </div>
-        </article>
+        <div className="place-card__rating rating">
+          <div className="place-card__stars rating__stars">
+            <span style={{width: `${props.rating}%`}}></span>
+            <span className="visually-hidden">{props.rating}</span>
+          </div>
+        </div>
+        <h2 className="place-card__name">
+          <a href="#">{props.title}</a>
+        </h2>
+        <p className="place-card__type">{props.type}</p>
+      </div>
+    </article>
+  );
 
-    );
+};
 
+Place.propTypes = {
+  title: PropTypes.string.isRequired,
+  image: PropTypes.string.isRequired,
+  type: PropTypes.string.isRequired,
+  price: PropTypes.number.isRequired,
+  rating: PropTypes.number,
+  isPremium: PropTypes.bool
 };
 
 export default Place;
