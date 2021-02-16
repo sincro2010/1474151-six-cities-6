c21d622a2b1f134a507f50ee3e22736d4342527a
sincro2010 <svobodagrani@gmail.com>
2021-02-16 19:23:32 +0500

    Устраняю пробелы у = в html коде

--- src/components/main-page/main-page.jsx
+++ src/components/main-page/main-page.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import PropTypes from "prop-types";
-import Place from "../place/place";
+import PropTypes from 'prop-types';
+import Place from '../place/place';
 
 const MainPage = (props) => {
   const {propertiesNumber} = props;
