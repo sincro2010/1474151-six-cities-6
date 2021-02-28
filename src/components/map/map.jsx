c0920add2a65e5a63afee67d809457042563e4e2
sincro2010 <svobodagrani@gmail.com>
2021-02-28 19:49:20 +0500

    Вопрос по карте

--- src/components/map/map.jsx
+++ src/components/map/map.jsx
@@ -1,44 +1,66 @@
-/* import React, {createRef} from 'react';
+import React, {useRef, useEffect} from 'react';
 import leaflet from 'leaflet';
 import 'leaflet/dist/leaflet.css';
 import placesPropTypes from '../../common/prop-types.js';
 
 const Map = (props) => {
   const {offers} = props;
+  const mapRef = useRef();
+  
+  const id = [];
+  id.push(offers.id);
 
-  const mapRef = createRef();
+    const city = [52.38333, 4.9];
+    const zoom = 12;
 
-  const city = [52.38333, 4.9];
-  const zoom = 12;
+    const icon = leaflet.icon({
+      iconUrl: `img/pin.svg`,
+      iconSize: [30, 30]
+    });
 
-  const map = leaflet.map(`map`, {
-    center: city,
-    zoom,
-    zoomControl: false,
-    marker: true
-  });
-  map.setView(city, zoom);
+    const useEffect = (() => {
 
+    mapRef.current = leaflet.map(`map`, {
+      center: city,
+      zoom,
+      zoomControl: false,
+      marker: true
+    });
+    mapRef.current.setView(city, zoom);
 
-  leaflet
-   .tileLayer(`https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png`, {
-     attribution: `&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>`
-   })
-   .addTo(map);
+  
+    leaflet
+     .tileLayer(`https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png`, {
+       attribution: `&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>`
+     })
+     .addTo(mapRef.current);
+  
+  
 
-   const icon = leaflet.icon({
-    iconUrl: `img/pin.svg`,
-    iconSize: [30, 30]
-  });
+const offerCords = [52.3709553943508, 4.89309666406198];
+leaflet
+ .marker(offerCords, {icon})
+ .addTo(mapRef.current);
 
+/*
+  offers.map((place) => {
+    leaflet
+    .marker({
+      latitude: place.location.latitude, 
+      longitude: place.location.longitude},
+      {icon})
+    .addTo(map);
 
-  const offerCords = [52.3709553943508, 4.89309666406198];
-  leaflet
-  .marker(offerCords, {icon})
-  .addTo(map);
+  }); */
+  
+}, []);
+  
+
+ 
 
   return (
-    <div id="map" ref={mapRef} style={{height: `auto`, width: `628px`}}></div>
+    <div id="map" ref={mapRef} style={{height: `auto`, width: `auto`}}></div>
+
   );
 };
 
@@ -46,4 +68,4 @@ Map.propTypes = {
   offers: placesPropTypes
 };
 
-export default Map; */
+export default Map;
